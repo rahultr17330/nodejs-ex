3f7c4aae64f6ad4dbdb15014f15abc49bcf554cd
rahul <rahultri17330@gmail.com>
2018-03-28 01:51:38 +0530

    commit

--- server.js
+++ server.js
@@ -59,7 +59,20 @@ var initDb = function(callback) {
     console.log('Connected to MongoDB at: %s', mongoURL);
   });
 };
-
+app.get('/pagecount', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    db.collection('counts').count(function(err, count ){
+      res.send('{ pageCount: ' + count + '}');
+    });
+  } else {
+    res.send('{ pageCount: -1 }');
+  }
+});
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
 });
