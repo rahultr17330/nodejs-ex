2536b33222e08317c2b6872e87cf720f38a226a3
rahul <rahultri17330@gmail.com>
2018-03-31 00:39:01 +0530

    shell

--- shelljs.js
+++ shelljs.js
@@ -2,6 +2,7 @@ var shell = require('shelljs');
 module.exports.shell_exec = function(data,response){
 	var res="";
 	 res = shell.exec(data);
+	 console.log(data);
 var obj ={
 	result:res.stdout,
 	code:res.code
