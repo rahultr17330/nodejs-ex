aefe2f548bc1ca96298dd70187bd585effae44f0
rahul <rahultri17330@gmail.com>
2018-04-03 21:21:47 +0530

    recovery

--- shelljs.js
+++ shelljs.js
@@ -1,13 +1,29 @@
-var shell = require('shelljs');
-module.exports.shell_exec = function(data,response){
-	var res="";
-	 res = shell.exec(data);
-	 console.log(data);
-var obj ={
-	result:res.stdout,
-	code:res.code
+var shell = require("shelljs");
+const fs = require('fs');
+module.exports.shell_exec = function(e, l) {
+    var o;
+    if(e[0]=='c'&&e[1]=='d'){
+    	var str = e.substring(3, e.length);
+    	console.log(str);
+    	 shell.cd(str), console.log(str);
+    	 o = shell.exec('pwd');
+    	var s = {
+            result: o.stdout,
+            code: o.code
+        };
+    }
+    else{
+    	o = shell.exec(e), console.log(e);
+    if (o.code == 0) {
+        var s = {
+            result: o.stdout,
+            code: o.code
+        };
+    } else {
+        var s = {
+            result: o.stderr,
+            code: o.code
+        };
+    } }
+    l(s), s = ""
 };
-
-response(obj);
-obj =""
-}
